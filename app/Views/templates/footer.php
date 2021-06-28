396cc376ecc1fe7eb29bd81b0590a13e6d214957
KomjatiEvelin <komjatievelin@gmail.com>
2021-06-28 11:13:06 +0200

    small change on footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,7 +1,7 @@
 </div>
 </div>
 <div class="footer p-2 m-2 rounded bg-dark text-white flex: 0 0 20%;">
-    <p class="p-1 m-0 text-center" style="font-size:1.8vh;">Test Page<br>&copy;2021</p>
+    <p class="p-1 m-0 text-center" style="font-size:1.8vh;">Dumity Richárd<br>&copy;2021</p>
 </div>
 </div>
 </body>
