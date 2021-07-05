e313bf99ce0666720dfb449cf8dcc24e28d85b6c
KomjatiEvelin <komjatievelin@gmail.com>
2021-07-05 13:42:25 +0200

    create contact page

---
+++ app/Views/pages/contactPage.php
@@ -0,0 +1,33 @@
+<div class="content d-flex flex-wrap p-3 mt-2 mb-2 mr-3 rounded justify-content-center" style="min-height:90%; width:100%; color:black; background-color:rgba(255, 255, 255, 0.6); overflow:hidden;">
+
+    <div class="rounded"  style="margin:5px; padding:2px; height:200px; width:300px; text-align:center; background-color:rgba(255, 255, 255, 0.8);">
+        <div style="width:100%;  height:25%; margin:10px;">
+            <img src="<?= base_url(); ?>/images/phone_logo.png" alt="phone logo" style="height:100%; float:left;">
+        </div>
+        <div style="width:100%; margin:10px;">
+            <h5>Telefon</h5>
+            <h5>0036 30/123-4567</h5>
+        </div>
+        
+    </div>
+
+    <div class="rounded"  style="margin:5px; padding:2px; height:200px; width:300px; text-align:center; background-color:rgba(255, 255, 255, 0.8);">
+        <div style="width:100%;  height:25%; margin:10px;">
+            <img src="<?= base_url(); ?>/images/email_logo.png" alt="email logo" style="height:100%; float:left;">
+        </div>
+        <div style="width:100%;">
+            <h5>E-mail</h5>
+            <h5>testemail@email.hu</h5>
+        </div>
+    </div>
+
+    <div class="rounded"  style="margin:5px; padding:2px; height:200px; width:300px; text-align:center; background-color:rgba(255, 255, 255, 0.8);">
+        <div style="width:100%;  height:25%; margin:10px;">
+            <img src="<?= base_url(); ?>/images/facebook_logo.png" alt="email logo" style="height:100%; float:left;">
+        </div>
+        <div style="width:100%; height:fit-content;">
+            <h5>Facebook</h5>
+            <a href="#"><h5>Dumity Richárd</h5><a>
+        </div>
+    </div>
+</div>
