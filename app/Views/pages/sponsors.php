04967737a3008557da0454da1fd19c471ee0d508
KomjatiEvelin <komjatievelin@gmail.com>
2021-07-09 11:30:19 +0200

    adding some style

--- app/Views/pages/sponsors.php
+++ app/Views/pages/sponsors.php
@@ -6,22 +6,22 @@
         $size;
         switch($datas_item['level']){
             case 1: 
-                $size=100;
+                $size=50;
                 break;
             case 2:
-                $size=130;
+                $size=80;
                 break;
             case 3:
-                $size=170;
+                $size=110;
                 break;
             case 4:
-                $size=210;
+                $size=140;
                 break;
             case 5:
-                $size=250;
+                $size=170;
                 break;
             default:
-                $size=100;
+                $size=50;
                 break;
             }
         if($datas_item['level']<10): ?>
@@ -29,7 +29,7 @@
         <div class="rounded sponsor-container"  style="margin:5px; padding:2px; height:fit-content; min-width:25%; max-width:40%; text-align:center;">
         <div onclick="openModal('myModal'+<?php echo $datas_item['id'] ?>);">
             <h5 style="font-weight:bold;"><?= esc($datas_item['name']) ?></h5>
-            <img src="<?= base_url(); ?>/images/sponsor_logos/<?= esc($datas_item['img'])?>" alt="szponzor logo" style="width:<?= $size ?>px; max-width:100%;">
+            <img src="<?= base_url(); ?>/images/sponsor_logos/<?= esc($datas_item['img'])?>" alt="szponzor logo" style="height:<?= $size ?>px; max-width:100%;">
             <p style="overflow:hidden; word-break: normal; max-height:5vh;"><?= esc($datas_item['info']) ?></p>
             <p style="color:blue;">... Részletek</p></div>
             <?php if(session()->get('username')=="admin"){
@@ -43,9 +43,9 @@
                 }
             ?>
         </div>
-        <div id="myModal<?= $datas_item['id'] ?>" class="modal" style=" overflow-y:auto;">
+        <div id="myModal<?= $datas_item['id'] ?>" class="modal" style="color:black; overflow-y:auto;">
             <span class="close cursor" onclick="closeModal('myModal'+<?php echo $datas_item['id'] ?>)">&times;</span>
-            <div class="modal-content" style="text-align:center; word-wrap: color:black; break-word;">
+            <div class="modal-content" style="text-align:center; word-wrap: break-word;">
             <?php $key = array_search($datas_item['id'], array_column($datas, 'id')); ?>
             <h5 style="font-weight:bold;"><?php echo $datas[$key]['name'] ?></h5>
             <img src="<?= base_url(); ?>/images/sponsor_logos/<?= esc($datas_item['img'])?>" alt="szponzor logo" style="margin-left: auto; margin-right: auto; width:300px; max-width:90%; padding:5px;">
